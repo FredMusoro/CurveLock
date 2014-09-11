a6a75c8bfb0966c50a7f12cf1b8cf7ef7223759b
Adam Caudill <adam@adamcaudill.com>
2014-09-11 01:27:57 -0400

    Add some basic unit tests

---
+++ src/Tests/KeyGenerationTest.cs
@@ -0,0 +1,45 @@
+﻿using CurveLock.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sodium;
+
+namespace Tests
+{
+  /// <summary>
+  ///This is a test class for KeyGenerationTest and is intended
+  ///to contain all KeyGenerationTest Unit Tests
+  ///</summary>
+  [TestClass()]
+  public class KeyGenerationTest
+  {
+    /// <summary>
+    ///A test for EncodePublicKey
+    ///</summary>
+    [TestMethod()]
+    public void EncodePublicKeyTest()
+    {
+      var publicKey = Utilities.HexToBinary("34f066fa6957e569848e7d6d1faaa129fe43e7bb6b4047e1db34d4e09a9f4e51");
+      const string EXPECTED = "Lqm2Foa9keELvGf8cYHJB3CRzEgPLuxJnBdtbZRbhuGmx3dhQB";
+
+      var actual = KeyGeneration.EncodePublicKey(publicKey);
+
+      Assert.AreEqual(EXPECTED, actual);
+    }
+
+    /// <summary>
+    ///A test for ScryptGenerateKey
+    ///</summary>
+    [TestMethod()]
+    public void ScryptGenerateKeyTest()
+    {
+      const string EMAIL = "bob@example.com";
+      const string PASSWORD = "bobIsGreat&1337!&sTuFf";
+      var expected = new KeyPair(
+        Utilities.HexToBinary("34f066fa6957e569848e7d6d1faaa129fe43e7bb6b4047e1db34d4e09a9f4e51"), 
+        Utilities.HexToBinary("4ba8e0b41fc07b5a1d4e0be0a4b464f6f2673a3854f967b5c31102b15683a195"));
+
+      //this may fail due to needing to allocate 1GB of RAM
+      var actual = KeyGeneration.ScryptGenerateKey(EMAIL, PASSWORD);
+      CollectionAssert.AreEqual(expected.PrivateKey, actual.PrivateKey);
+    }
+  }
+}
---
+++ src/Tests/MessageCryptoTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using CurveLock.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Sodium;
+
+namespace Tests
+{
+  /// <summary>
+  ///This is a test class for MessageCryptoTest and is intended
+  ///to contain all MessageCryptoTest Unit Tests
+  ///</summary>
+  [TestClass()]
+  public class MessageCryptoTest
+  {
+    /// <summary>
+    ///A test for DecryptMessage
+    ///</summary>
+    [TestMethod()]
+    public void DecryptMessageTest()
+    {
+      var message = Base58Check.Base58CheckEncoding.Decode("1AtPS411ipfcRmvBe1hoSvAbJpNfpvpzKrQHTuK8jw9WTGf69FM7kmkDxhf9nFpfG4dbEgrxYMuyyR3x4LM8LbscDx3x5mQWmGL8cDCShJKFBeEqN688QfhGNVVTsKqWNUJ");
+      var privateKey = Utilities.HexToBinary("4ba8e0b41fc07b5a1d4e0be0a4b464f6f2673a3854f967b5c31102b15683a195");
+      var expected = Encoding.UTF8.GetBytes("bob");
+      var actual = MessageCrypto.DecryptMessage(message, privateKey);
+
+      CollectionAssert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
+    ///A test for EncryptMessage
+    ///</summary>
+    [TestMethod()]
+    public void EncryptMessageTest()
+    {
+      var message = Encoding.UTF8.GetBytes("bob");
+      const string TO_ID = "Lqm2Foa9keELvGf8cYHJB3CRzEgPLuxJnBdtbZRbhuGmx3dhQB";
+      var expected = Encoding.UTF8.GetBytes("bob");
+      var encrypted = MessageCrypto.EncryptMessage(message, TO_ID);
+
+      var actual = MessageCrypto.DecryptMessage(encrypted, 
+        Utilities.HexToBinary("4ba8e0b41fc07b5a1d4e0be0a4b464f6f2673a3854f967b5c31102b15683a195"));
+
+      CollectionAssert.AreEqual(expected, actual);
+    }
+  }
+}
