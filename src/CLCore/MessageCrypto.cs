928d8d6a7d418c52286afd0b0c1de1e986a57495
Adam Caudill <adam@adamcaudill.com>
2014-09-16 13:08:33 -0400

    Cleanup

--- src/CLCore/MessageCrypto.cs
+++ src/CLCore/MessageCrypto.cs
@@ -9,14 +9,14 @@ namespace CurveLock.Core
     public static byte[] EncryptMessage(byte[] message, string toId)
     {
       //generate an ephemeral key to encrypt this with
-      var key = Sodium.PublicKeyBox.GenerateKeyPair();
+      var key = PublicKeyBox.GenerateKeyPair();
 
       //TODO: Check to make sure that the ID version is 0x0A
       var recip = ArrayHelpers.SubArray(Base58Check.Base58CheckEncoding.Decode(toId), 1);
 
-      var nonce = Sodium.SodiumCore.GetRandomBytes(24);
-      var encrypted = Sodium.PublicKeyBox.Create(message, nonce, key.PrivateKey, recip);
-      var recipVerifier = Sodium.GenericHash.Hash(ArrayHelpers.ConcatArrays(nonce, key.PublicKey), null, 16);
+      var nonce = SodiumCore.GetRandomBytes(24);
+      var encrypted = PublicKeyBox.Create(message, nonce, key.PrivateKey, recip);
+      var recipVerifier = GenericHash.Hash(ArrayHelpers.ConcatArrays(nonce, key.PublicKey), null, 16);
       var version = new byte[] { 0x00 };
 
       var final = ArrayHelpers.ConcatArrays(version, nonce, key.PublicKey, recipVerifier, encrypted);
@@ -42,7 +42,7 @@ namespace CurveLock.Core
           var verifier = ArrayHelpers.SubArray(message, 57, RECIP_VERIFIER_LENGTH);
           var encrypted = ArrayHelpers.SubArray(message, 73);
 
-          var decrypted = Sodium.PublicKeyBox.Open(encrypted, nonce, privateKey, senderKey);
+          var decrypted = PublicKeyBox.Open(encrypted, nonce, privateKey, senderKey);
           ret = decrypted;
 
           break;
@@ -57,7 +57,7 @@ namespace CurveLock.Core
     public static string EncryptFile(string path, string toId)
     {
       //generate an ephemeral key to encrypt this with
-      var key = Sodium.PublicKeyBox.GenerateKeyPair();
+      var key = PublicKeyBox.GenerateKeyPair();
 
       //TODO: Check to make sure that the ID version is 0x0A
       var recip = ArrayHelpers.SubArray(Base58Check.Base58CheckEncoding.Decode(toId), 1);
